f4c72144efbc83d08b998d61e273d4f3cb777715
kongben <1317495859@qq.com>
2020-07-20 21:52:45 +0800

    add menu

--- src/layout/components/Sidebar.vue
+++ src/layout/components/Sidebar.vue
@@ -1,9 +1,60 @@
 <template>
   <div>
-    <div class="el-scrollbar">sidebar</div>
+    <div class="el-scrollbar">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#304156"
+        text-color="#fff"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+
+          <el-menu-item index="1-3">选项3</el-menu-item>
+
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
   </div>
 </template>
-
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  },
+  components: {},
+  computed: {},
+  methods: {
+    handleOpen(res) {
+      console.log(res);
+    },
+    handleClose(res) {
+      console.log(res);
+    }
+  }
+};
+</script>
 <style scoped>
-
 </style>
